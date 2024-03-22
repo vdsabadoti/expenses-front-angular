8a7e35b54fc68532664b860ddad98d49eb7687e9
Viviane DEROLDO SABADOTI <viviane.deroldosabadoti2023@campus-eni.fr>
2024-03-22 12:59:03 +0100

    New User in progress

--- src/components/new-user/new-user.component.ts
+++ src/components/new-user/new-user.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule
+    ],
   templateUrl: './new-user.component.html',
   styleUrl: './new-user.component.css'
 })
 export class NewUserComponent {
 
+  private router:Router = inject(Router);
+
+  public text:string | undefined;
+
+  createNewUser() {
+
+  }
+
+  cancelCreation() {
+    this.router.navigate(['/login'])
+  }
 }
